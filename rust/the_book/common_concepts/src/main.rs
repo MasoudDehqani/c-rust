fd329052dbac3827adfa0d496889fd1e4c7fad7c
Masoud <masoud67.md@gmail.com>
2025-08-20 14:50:02 +0330

    rust: explanations added to common_programming_concepts

--- rust/the_book/common_concepts/src/main.rs
+++ rust/the_book/common_concepts/src/main.rs
@@ -1,7 +1,12 @@
 /*
     - values (variable) which are bind to a name or identifier are
     by default immutable in Rust
-    - immutability contributes to safety and easy concurrency
+    - immutability contributes to safety and easy concurrency that Rust provides
+
+    - by using 'mut' keyword in variable definition, readers of the code
+    be informed that the variable's value can be changed by other parts of the code
+
+
 */
 
 fn main() {
