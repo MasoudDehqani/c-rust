f58fcf335a95671781fcf84cab5769af7a3f08f4
Masoud <masoud67.md@gmail.com>
2025-08-23 11:07:24 +0330

    rust: explanations added to the mini_grep

--- rust/the_book/command_line_program/src/lib.rs
+++ rust/the_book/command_line_program/src/lib.rs
@@ -7,6 +7,21 @@ pub struct Config {
     pub file_path: String,
 }
 
+/*
+    - without using 'static lifetime specifier in the build function's signature, the compiler
+    does not know where to borrow the &str (which is a borrowed type). There is no string related
+    type in the params of the function and there is only a &str in the return type. So, when there is
+    no related type in params, it will be defined in the function itself and by the end of the
+    function execution, all defined variables will be deallocated. So, the compiler does not know
+    where to borrow the &str in return type. By specifying 'static lifetime specifier, the compiler
+    is informed that the string literal value would be in the binary which is available for the
+    entire life of the program. In general, string literals are global and live forever. The 'static
+    lifetime specifier also has the exact meaning
+
+    - using 'mut' before the parameter, does not mean that the caller has to pass a mutable arg.
+    it means, inside the function body, the binding named args can be mutated.
+    - in Rust, mutability is not part of the type, it is part of the binding
+*/
 impl Config {
     pub fn build<T: Iterator<Item = String>>(mut args: T) -> Result<Config, &'static str> {
         args.next();
