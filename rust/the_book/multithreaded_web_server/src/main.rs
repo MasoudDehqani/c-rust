09cda7a928a805b8de9501b2f6e11ad7a13c34db
Masoud <masoud67.md@gmail.com>
2025-09-12 16:24:44 +0330

    Rust: codes added to the web server chapter

--- rust/the_book/multithreaded_web_server/src/main.rs
+++ rust/the_book/multithreaded_web_server/src/main.rs
@@ -2,23 +2,61 @@ use std::{
     fs,
     io::{BufRead, BufReader, Read, Write},
     net::{TcpListener, TcpStream},
+    thread,
+    time::Duration,
 };
 
 fn main() {
     let listener = TcpListener::bind("127.0.0.1:7878").unwrap();
-
     listener
         .incoming()
-        .for_each(|s| handle_connection_with_buffer(s.unwrap()));
+        .for_each(|s| handle_connection(s.unwrap()));
+}
+
+fn handle_connection(mut stream: TcpStream) {
+    let buf = BufReader::new(&stream);
+    let req_line = buf.lines().next().unwrap().unwrap();
+
+    let (path, status_line) = match req_line.as_str() {
+        "GET / HTTP/1.1" => ("hello.html", "HTTP/1.1 200 OK"),
+        _ => ("404.html", "HTTP/1.1 404 NOT FOUND"),
+    };
+
+    let content = fs::read_to_string(path).unwrap();
+    let content_length = content.len();
+    let res = format!("{status_line}\r\nContent-Length: {content_length}\r\n\r\n{content}");
+
+    stream.write_all(res.as_bytes()).unwrap();
+}
+
+fn _handle_connection_with_blocking(mut stream: TcpStream) {
+    let buf = BufReader::new(&stream);
+    let req_line = buf.lines().next().unwrap().unwrap();
+
+    let (path, status_line) = match req_line.as_str() {
+        "GET / HTTP/1.1" => ("hello.html", "HTTP/1.1 200 OK"),
+        "GET /blocking HTTP/1.1" => {
+            thread::sleep(Duration::from_secs(5));
+            ("hello.html", "HTTP/1.1 200 OK")
+        }
+        _ => ("404.html", "HTTP/1.1 404 NOT FOUND"),
+    };
+
+    let content = fs::read_to_string(path).unwrap();
+    let content_length = content.len();
+    let res = format!("{status_line}\r\nContent-Length: {content_length}\r\n\r\n{content}");
+
+    stream.write_all(res.as_bytes()).unwrap();
 }
 
 /*
   - the kernel receives the packet data from the NIC -> stores it in its own buffer
   - the program then create its own buffer -> data is copied into the program's buffer in user space
   - a buffer is just a chunk of memory where you temporarily store data while moving moving it
-between two places
+  between two places
+  - NIC -> network interface controller (network interface card)
 */
-fn handle_connection_with_buffer(mut stream: TcpStream) {
+fn _handle_connection_with_buffer(mut stream: TcpStream) {
     let buf = BufReader::new(&stream);
     let req_line = buf.lines().next().unwrap().unwrap();
 
@@ -44,12 +82,12 @@ fn handle_connection_with_buffer(mut stream: TcpStream) {
 }
 
 /*
-  this code makes the server wait for an EOF. here tcp stream in the keep-alive connection does not
+  This code makes the server wait for an EOF. Here the tcp stream in the keep-alive connection does not
   have an EOF
   - EOF -> end of file
   - end of file in a network connection means end of bytes
   - fs::read_to_string method wait for an EOF which is not available in a tcp keep-alive connection
-  - here we need a buffer reader which reads up to the end of each buffers come the server
+  - here we need a buffer reader which reads up to the end of each buffers coming to the server
   - a buffer is just a chunk of memory where you temporarily store data while moving moving it
   between two places
 */
