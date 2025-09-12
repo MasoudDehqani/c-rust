3b546b6fef7c7443e3cc28508393842d439533de
Masoud <masoud67.md@gmail.com>
2025-09-12 15:53:34 +0330

    Rust: initial codes added to the web server chapter, explanations added to asynchronous programming chapter

--- rust/the_book/asynchronous_programming/src/main.rs
+++ rust/the_book/asynchronous_programming/src/main.rs
@@ -1,6 +1,12 @@
 /*
     - communication over channel VS communication over static memory (if the data is read-only)
+    - awaiting on a join handle is one way of communicate the outcome of the spawned operation
+    back to the caller
+
+    - ??? spawning in tokio -> spawning a future ???
+    - spawning from std::thread -> spawning a thread
 */
+
 fn main() {
     println!("Hello, world!");
 }
--- rust/the_book/multithreaded_web_server/src/main.rs
+++ rust/the_book/multithreaded_web_server/src/main.rs
@@ -1,1 +1,83 @@
-fn main() {}
+use std::{
+    fs,
+    io::{BufRead, BufReader, Read, Write},
+    net::{TcpListener, TcpStream},
+};
+
+fn main() {
+    let listener = TcpListener::bind("127.0.0.1:7878").unwrap();
+
+    listener
+        .incoming()
+        .for_each(|s| handle_connection_with_buffer(s.unwrap()));
+}
+
+/*
+  - the kernel receives the packet data from the NIC -> stores it in its own buffer
+  - the program then create its own buffer -> data is copied into the program's buffer in user space
+  - a buffer is just a chunk of memory where you temporarily store data while moving moving it
+between two places
+*/
+fn handle_connection_with_buffer(mut stream: TcpStream) {
+    let buf = BufReader::new(&stream);
+    let req_line = buf.lines().next().unwrap().unwrap();
+
+    match req_line.as_str() {
+        "GET / HTTP/1.1" => {
+            let content = fs::read_to_string("hello.html").unwrap();
+            let content_length = content.len();
+            let content =
+                format!("HTTP/1.1 200 OK\r\nContent-Length: {content_length}\r\n\r\n{content}");
+
+            stream.write_all(content.as_bytes()).unwrap();
+        }
+        _ => {
+            let content = fs::read_to_string("404.html").unwrap();
+            let content_length = content.len();
+            let content = format!(
+                "HTTP/1.1 404 NOT FOUND\r\nContent-Length: {content_length}\r\n\r\n{content}"
+            );
+
+            stream.write_all(content.as_bytes()).unwrap();
+        }
+    }
+}
+
+/*
+  this code makes the server wait for an EOF. here tcp stream in the keep-alive connection does not
+  have an EOF
+  - EOF -> end of file
+  - end of file in a network connection means end of bytes
+  - fs::read_to_string method wait for an EOF which is not available in a tcp keep-alive connection
+  - here we need a buffer reader which reads up to the end of each buffers come the server
+  - a buffer is just a chunk of memory where you temporarily store data while moving moving it
+  between two places
+*/
+fn _handle_connection_with_read_to_string(mut stream: TcpStream) {
+    let mut s = String::new();
+    stream.read_to_string(&mut s).unwrap();
+
+    match s.lines().next() {
+        Some(status) => {
+            if status == "GET / HTTP/1.1" {
+                let content = fs::read_to_string("hello.html").unwrap();
+                let content_length = content.len();
+                let res =
+                    format!("HTTP/1.1 200 OK\r\nContent-Length: {content_length}\r\n\r\n{content}");
+                println!("{}", res);
+
+                stream.write_all(res.as_bytes()).unwrap();
+            } else {
+                let content = fs::read_to_string("404.html").unwrap();
+                let content_length = content.len();
+                let res = format!(
+                    "HTTP/1.1 404 NOT FOUND\r\nContent-Length: {content_length}\r\n\r\n{content}"
+                );
+
+                stream.write_all(res.as_bytes()).unwrap();
+            }
+        }
+
+        None => println!("ERROR"),
+    };
+}
