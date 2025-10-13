2bf77d701ad7ed5472790e029032cb6e8ce511e4
Masoud <masoud67.md@gmail.com>
2025-10-13 07:45:39 +0330

    Rust: codes added to async programming of the book

--- rust/the_book/asynchronous_programming/src/main.rs
+++ rust/the_book/asynchronous_programming/src/main.rs
@@ -1,63 +1,15 @@
-use futures::future::{self, Either};
-use reqwest;
-use scraper::Html;
-use std::pin::pin;
-use tokio::{self, runtime::Runtime};
+#[allow(unused_imports)]
+use asynchronous_programming::{
+    counter_with_task, get_one_title_and_print, race_between_two_title,
+};
+use tokio::{self};
 
 #[tokio::main]
 async fn main() {
-    let args: Vec<String> = std::env::args().collect();
-    let url1 = &args[1];
-    let url2 = &args[2];
-
-    // let maybe_title_1 = get_title(url1).await;
-    // let maybe_title_2 = get_title(url2).await;
-
-    match select(get_title(url1), get_title(url2)).await {
-        Either::Left(mut title1) => println!(
-            "The title is : {}",
-            title1.get_or_insert("NO TITLE".to_string())
-        ),
-        Either::Right(mut title2) => println!(
-            "The title is : {}",
-            title2.get_or_insert("NO TITLE".to_string())
-        ),
-    }
-
-    // match maybe_title {
-    //     Some(title) => println!("The title is : {title}"),
-    //     None => println!("The url {url} had no title"),
-    // }
-
-    // let runtime = Runtime::new().unwrap();
-    // runtime.block_on(async {
-    //     let maybe_title = get_title(url).await;
-    //     match maybe_title {
-    //         Some(title) => println!("The title is : {title}"),
-    //         None => println!("The url {url} had no title"),
-    //     }
-    // });
+    race_between_two_title().await;
 }
 
-async fn get_title(url: &str) -> Option<String> {
-    let response = reqwest::get(url).await.unwrap();
-    let response_text = response.text().await.unwrap();
-    Html::parse_document(&response_text)
-        .select(&scraper::Selector::parse("title").unwrap())
-        .nth(0)
-        .map(|t| t.inner_html())
-}
-
-async fn select<A, B, F1, F2>(f1: F1, f2: F2) -> Either<A, B>
-where
-    F1: Future<Output = A>,
-    F2: Future<Output = B>,
-{
-    let f1 = pin!(f1);
-    let f2 = pin!(f2);
-
-    match future::select(f1, f2).await {
-        Either::Left((a, _)) => Either::Left(a),
-        Either::Right((b, _)) => Either::Right(b),
-    }
-}
+// fn main() {
+//     get_one_title_and_print();
+//     counter_with_task();
+// }
